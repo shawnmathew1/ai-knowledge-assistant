7dfcbdf3a58e4eb4e320fc4f9823911b033cd1bc
shawnmathew1 <shawneapenmathew@gmail.com>
2026-01-07 16:38:29 -0500

    Add file chunking to upload route

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
+
 app.use(express.json());
+
+
+
+
+function chunkText(text, size = 300) {
+    const chunks = [];
+    for (let i = 0; i < text.length; i += size) {
+        chunks.push(text.slice(i, i + size));
+    }
+    return chunks;
+}
 
 
 const storage = multer.diskStorage({
@@ -23,10 +37,22 @@ app.get("/health", (req, res) => {
 
 
 app.post("/upload", upload.single("document"), (req, res) => {
+
     if (!req.file) {
-        return res.status(400).json({ error: "No file uploaded" });
+        return res.status(400).json({ error: "No file  uploaded" });
     }
-    res.json({ message: "File uploaded sucessfully", filename: req.file.originalname});
+
+    const filePath = path.join(__dirname, "uploads", req.file.filename);
+
+    const fileContents = fs.readFileSync(filePath, "utf-8");
+
+    const chunks = chunkText(fileContents);
+
+    console.log("File contents: ", fileContents);
+
+    console.log("Chunks:", chunks);
+
+    res.json({ message: "File uploaded and processed", filename: req.file.originalname});
 });
 
 app.listen(3001, () => {
